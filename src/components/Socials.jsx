c5539961f7f1ae53c7750bd6f6aea658969a6834
Bruno Furtado <bsouzacc@gmail.com>
2024-08-14 05:00:23 -0300

    update /pages

--- src/components/Socials.jsx
+++ src/components/Socials.jsx
@@ -9,22 +9,22 @@ export const Socials = () => {
   return (
     <ul className="socials">
       <li className="socials__item">
-        <a href="https://x.com/marvelstudios" target="_blank" className="socials__link">
+        <a href="https://x.com/marvelstudios" target="_blank" className="socials__link" aria-label="rede social link ">
           <FaSquareXTwitter />
         </a>
       </li>
       <li className="socials__item">
-        <a href="https://www.facebook.com/marvelstudios/" target="_blank" className="socials__link">
+        <a href="https://www.facebook.com/marvelstudios/" target="_blank" className="socials__link" aria-label="rede social link ">
           <ImFacebook2 />
         </a>
       </li>
       <li className="socials__item">
-        <a href="https://www.youtube.com/@marvel" target="_blank" className="socials__link">
+        <a href="https://www.youtube.com/@marvel" target="_blank" className="socials__link" aria-label="rede social link ">
           <IoLogoYoutube />
         </a>
       </li>
       <li className="socials__item">
-        <a href="https://www.instagram.com/marvelstudios/" target="_blank" className="socials__link">
+        <a href="https://www.instagram.com/marvelstudios/" target="_blank" className="socials__link" aria-label="rede social link ">
           <FaInstagramSquare />
         </a>
       </li>
